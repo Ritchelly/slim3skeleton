301b65ab486fcb5dfd4e9824610bf08119ac99a1
Ritchelly <ritch.sa@hotmail.com>
2020-09-03 21:50:52 -0300

    [ AMI ] - First routes with AMI.

--- src/routes.php
+++ src/routes.php
@@ -4,13 +4,31 @@ use Slim\App;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+use App\Ami;
+
 require '../vendor/autoload.php';
 
 return function ( App $app ) {
 
-    $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $app->get('/queue/{name}', function (Request $request, Response $response, array $args) {
         $name = $args['name'];
-        $response->getBody()->write("Hello, $name");
+
+        $ami = new Ami();
+
+        $command  = [
+            "Action" =>"QueueStatus",
+            "ActionID"=> "sdasda",
+            "Queue" => "callcenter"
+        ];
+
+        $events = [
+            "QueueMember", 
+            "QueueParams"
+        ];
+
+        $rs = json_encode( $ami->sendAction($command,$events), JSON_PRETTY_PRINT);
+
+        $response->getBody()->write($rs);
     
         return $response;
     });
